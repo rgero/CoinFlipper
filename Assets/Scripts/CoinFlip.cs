8f503b9be50dd57808a02a6724062f386def8c6e
Roy Gero <roy.gero.ii@gmail.com>
2021-08-05 19:45:52 -0400

    Initial Commit. Right now it's a simple coin, simple flipping with hitting E. It'll reset awkwardly if you hit R though

---
+++ Assets/Scripts/CoinFlip.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinFlip : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Vector3 startPos;
+
+    void Start()
+    {
+        startPos = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            KickFlip();
+        }
+
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            ResetPosition();
+        }
+    }
+
+    void KickFlip()
+    {
+        Rigidbody rigidbody = this.GetComponent<Rigidbody>();
+        float forceAmount = Random.Range(20, 100);
+        Vector3 forcePosition = new Vector3(Random.Range(0, 10), Random.Range(0, 10), Random.Range(0, 10));
+        rigidbody.AddForceAtPosition(new Vector3(0, forceAmount, 0), forcePosition);
+    }
+
+    void ResetPosition()
+    {
+        this.transform.position = startPos;
+        this.transform.rotation.eulerAngles.Set(90, 0, 0);
+    }
+}
---
+++ Assets/Scripts/FollowCoin.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCoin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject coinObject;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.LookAt(coinObject.transform, Vector3.up);
+    }
+}
